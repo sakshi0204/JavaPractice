a2fa6f804514faf9cd347fedc60cd3d36fd831ff
Sakshi Narang <sakshinarang2802@gmail.com>
2025-06-30 14:31:51 +0530

    We write a Java program to print each character of a string one by one on a new line.  Eg: "Hello World"  H  e  l  l  o
     W
     o
     r
     l
     d

---
+++ src/PrintEachCharacter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PrintEachCharacter {
+
+    public static void main(String[] args) {
+        int i, c = 0;
+        String s1;
+
+        System.out.println("enter the string");
+        Scanner sc = new Scanner(System.in);
+        // s1=sc.next();//takes length for the first world before space
+        s1 = sc.nextLine();//takes length including space
+        //int s2=s1.length();
+
+        for (i = 0; i < s1.length(); i++) {
+
+            System.out.println(s1.charAt(i));
+
+        }
+
+    }
+}
+
